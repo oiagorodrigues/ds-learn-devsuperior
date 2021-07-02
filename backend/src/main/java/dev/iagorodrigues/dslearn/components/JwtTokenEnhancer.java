4f448f7130030ea39c9391756e38cbe5e8438688
Iago Rodrigues <yagomello16@gmail.com>
2021-07-02 17:49:17 -0300

    chore: implement security infrastructure

---
+++ backend/src/main/java/dev/iagorodrigues/dslearn/components/JwtTokenEnhancer.java
@@ -0,0 +1,32 @@
+package dev.iagorodrigues.dslearn.components;
+
+import dev.iagorodrigues.dslearn.entities.User;
+import dev.iagorodrigues.dslearn.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+    @Autowired private UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        User user = userRepository.findByEmail(authentication.getName());
+
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("userId", user.getId());
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+        token.setAdditionalInformation(userMap);
+
+        return accessToken;
+    }
+}
--- backend/src/main/java/dev/iagorodrigues/dslearn/entities/User.java
+++ backend/src/main/java/dev/iagorodrigues/dslearn/entities/User.java
@@ -1,5 +1,9 @@
 package dev.iagorodrigues.dslearn.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,13 +16,15 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -100,5 +106,38 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return getId().hashCode();
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
