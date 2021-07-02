da7d079b93c21ea510ed6a68854a600bfd690997
Iago Rodrigues <yagomello16@gmail.com>
2021-07-02 12:39:29 -0300

    init backend

---
+++ backend/src/test/java/dev/iagorodrigues/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iagorodrigues.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
