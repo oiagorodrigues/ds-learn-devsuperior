f58bfd1e71a46432f236fc98ec6cebf78b0895ef
Iago Rodrigues <yagomello16@gmail.com>
2021-07-02 17:25:07 -0300

    chore: add validations infrastrucre

---
+++ backend/src/main/java/dev/iagorodrigues/dslearn/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,66 @@
+package dev.iagorodrigues.dslearn.controllers.exceptions;
+
+import dev.iagorodrigues.dslearn.errors.StandardError;
+import dev.iagorodrigues.dslearn.errors.ValidationError;
+import dev.iagorodrigues.dslearn.services.exceptions.DatabaseException;
+import dev.iagorodrigues.dslearn.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        StandardError err = createError(exception, request, status, "Resource not found");
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(DatabaseException exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        StandardError err = createError(exception, request, status, "Database exception");
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validationException(MethodArgumentNotValidException exception,
+                                                               HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Validation exception");
+        err.setPath(request.getRequestURI());
+
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            err.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    private StandardError createError(Exception exception, HttpServletRequest request,
+                                      HttpStatus status, String error) {
+        StandardError standardError = new StandardError();
+        standardError.setTimestamp(Instant.now());
+        standardError.setStatus(status.value());
+        standardError.setError(error);
+        standardError.setMessage(exception.getMessage());
+        standardError.setPath(request.getRequestURI());
+        return standardError;
+    }
+}
---
+++ backend/src/main/java/dev/iagorodrigues/dslearn/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package dev.iagorodrigues.dslearn.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
